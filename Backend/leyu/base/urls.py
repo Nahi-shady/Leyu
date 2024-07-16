9b183c70f721cc0ff3c0145029b43bb2982e723d
Nahom Zewdu <nahiyo86@gmail.com>
2024-07-16 20:31:47 +0300

    Created a the url routing for all api endpoints

--- Backend/leyu/base/urls.py
+++ Backend/leyu/base/urls.py
@@ -1,1 +1,19 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import *
+
+router = DefaultRouter()
+router.register(r'children', ChildViewSet)
+router.register(r'parents', ParentViewSet)
+router.register(r'teachers', TeacherViewSet)
+
+router.register(r'courses', CourseViewSet)
+router.register(r'videos', VideoViewSet)
+router.register(r'pdfs', PDFViewSet)
+router.register(r'quizzes', QuizViewSet)
+router.register(r'enrollments', EnrollmentViewSet)
+router.register(r'child-progress', ChildProgressViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
