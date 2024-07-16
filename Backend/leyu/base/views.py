09758948755699e9bca9ef588a4cc21ce698c1bd
Nahom Zewdu <nahiyo86@gmail.com>
2024-07-16 20:30:49 +0300

    Created the view layer for all api requests

--- Backend/leyu/base/views.py
+++ Backend/leyu/base/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Parent, Child
+from .serializers import *
 
-# Create your views here.
+# CRUD API VIEW FOR PARENTS
+class ParentViewSet(viewsets.ModelViewSet):
+    queryset = Parent.objects.all()
+    serializer_class = ParentSerializer
+    lookup_field = 'user__username'
+
+# CRUD API VIEW FOR PARENTS
+class ChildViewSet(viewsets.ModelViewSet):
+    queryset = Child.objects.all()
+    serializer_class = ChildSerializer
+    lookup_field = 'user__username'
+
+# CRUD API VIEW FOR TEACHERS
+class TeacherViewSet(viewsets.ModelViewSet):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+    lookup_field = 'user_username'
+
+
+# API views for course, vidoes, pdfs, quize and enrollment and childporgress
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+class VideoViewSet(viewsets.ModelViewSet):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
+
+class PDFViewSet(viewsets.ModelViewSet):
+    queryset = PDF.objects.all()
+    serializer_class = PDFSerializer
+
+class QuizViewSet(viewsets.ModelViewSet):
+    queryset = Quiz.objects.all()
+    serializer_class = QuizSerializer
+
+class EnrollmentViewSet(viewsets.ModelViewSet):
+    queryset = Enrollment.objects.all()
+    serializer_class = EnrollmentSerializer
+
+class ChildProgressViewSet(viewsets.ModelViewSet):
+    queryset = ChildProgress.objects.all()
+    serializer_class = ChildProgressSerializer
