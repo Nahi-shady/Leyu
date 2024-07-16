15d1fe9cb25193391f35774981e5f89444b0c574
Nahom Zewdu <nahiyo86@gmail.com>
2024-07-16 20:08:56 +0300

    Created a model for Parent, Child and Teacher databes tables

--- Backend/leyu/base/models.py
+++ Backend/leyu/base/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Parent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Parent')
+    phone = models.CharField(max_length=100, verbose_name="Parent's Phone")
+    address = models.CharField(max_length=100, verbose_name='Address')
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.user.username
+
+class Child(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Child')
+    parent = models.ForeignKey(Parent, on_delete=models.CASCADE, related_name='children')
+    disability = models.CharField(max_length=100, verbose_name='Disability')
+    phone = models.CharField(max_length=100, verbose_name="Child's Phone")
+    age = models.IntegerField(null=False, blank=False, verbose_name="Child's Age")
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
+
+class Teacher(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Teacher')
+    phone = models.CharField(max_length=100, verbose_name="Phone")
+    address = models.CharField(max_length=100, verbose_name='Address')
+
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
